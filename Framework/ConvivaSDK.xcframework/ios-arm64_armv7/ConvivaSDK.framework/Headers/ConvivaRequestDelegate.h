7a49102161c2b5b96b774e73712891c40bc3f4ad
smadinen <smadine@conviva.com>
2021-12-08 23:12:03 +0530

    Release version 4.0.23

--- Framework/ConvivaSDK.xcframework/ios-arm64_armv7/ConvivaSDK.framework/Headers/ConvivaRequestDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-DEPRECATED_MSG_ATTRIBUTE("This class will be removed soon, please migrate to Simplified SDK APIs. Refer to https://community.conviva.com/site/one-sensor/sensors/ios/index_one_sensor.gsp")
-@protocol ConvivaRequestDelegate <NSObject>
-
-- (void)handleRequest:(NSURLRequest *)request
-    completionHandler:(void(^)(NSData *responseData, NSURLResponse *response, NSError *error))completionHandler DEPRECATED_ATTRIBUTE;
-
-@end
