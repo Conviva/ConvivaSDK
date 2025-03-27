4991f8ee1e5ab727a0bf391ef1898d600ff390dd
Butchi Peddi <bpeddi@conviva.com>
2025-03-27 23:06:18 +0530

    Release 4.1.2

--- Package.swift
+++ Package.swift
@@ -22,6 +22,16 @@ let package = Package(
         .binaryTarget(
             name: "ConvivaSDK",
             url: "https://github.com/Conviva/ConvivaSDK/raw/4.1.2/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "f4f93c1f44d4e662c7b22ce0b95184cf3b910e523e368a208271909ce970f2be")
+            checksum: "f4f93c1f44d4e662c7b22ce0b95184cf3b910e523e368a208271909ce970f2be"),
+linkerSettings: [
+                .linkedFramework("CoreMedia", .when(platforms: [.iOS,.tvOS])),
+                .linkedFramework("SystemConfiguration", .when(platforms: [.iOS,.tvOS])),
+		.linkedFramework("MobileCoreServices", .when(platforms: [.iOS,.tvOS])),
+                .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
+		.linkedFramework("Network", .when(platforms: [.iOS,.tvOS])),
+		.linkedFramework("AVFoundation", .when(platforms: [.iOS,.tvOS])),
+		.linkedFramework("AppTrackingTransparency", .when(platforms: [.iOS,.tvOS])),
+                .unsafeFlags(["-ObjC"], .when(platforms: [.iOS,.tvOS])),
+            ]
     ]
 )
